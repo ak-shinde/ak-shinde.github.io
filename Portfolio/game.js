fd5cf004725e3b2bbbeab5ea7ec398f33a2e16c8
Akshay Shinde <shindeak@usc.edu>
2023-01-11 18:38:44 +0530

    POC - disable link on touch

--- Portfolio/game.js
+++ Portfolio/game.js
@@ -279,7 +279,7 @@ document.querySelector(".dpad-select").addEventListener("touchstart", (e) => han
 // document.querySelector(".dpad-down").addEventListener("touchend", (e) => handleDpadPress(directions.down));
 // document.querySelector(".dpad-select").addEventListener("touchend", (e) => handleDpadPress(directions.select));
 
-document.querySelector(".map").addEventListener("contextmenu", (e) => e.preventDefault());
+document.querySelector(".frame").addEventListener("contextmenu", (e) => e.preventDefault());
 document.querySelector(".dpad-left").addEventListener("contextmenu", (e) => e.preventDefault());
 document.querySelector(".dpad-up").addEventListener("contextmenu", (e) => e.preventDefault());
 document.querySelector(".dpad-right").addEventListener("contextmenu", (e) => e.preventDefault());
